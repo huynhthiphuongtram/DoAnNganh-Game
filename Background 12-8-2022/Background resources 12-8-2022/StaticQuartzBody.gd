1bf5d5cb8819fd2494903a26abdd7bfed00313a3
Huỳnh Thị Phương Trâm <69827176+huynhthiphuongtram@users.noreply.github.com>
2022-09-18 12:32:06 +0700

    Add files via upload

--- Background 12-8-2022/Background resources 12-8-2022/StaticQuartzBody.gd
+++ Background 12-8-2022/Background resources 12-8-2022/StaticQuartzBody.gd
@@ -3,5 +3,6 @@ extends StaticBody2D
 var quartz_taken = false
 
 func _on_Area2D_body_entered(body: PhysicsBody2D):
-	print("ok")
+	quartz_taken = true
+	print(quartz_taken)
 	get_tree().change_scene("")
